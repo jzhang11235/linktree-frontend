6b0ff9a988b5791626ec76a59c340914181fe521
Jeffrey Zhang <jzhang11235@gmail.com>
2021-06-05 21:41:33 +1000

    Style classic link

--- src/components/links/ClassicLink.tsx
+++ src/components/links/ClassicLink.tsx
@@ -1,10 +1,29 @@
+import styled from 'styled-components';
+
 export type ClassicLinkProps = {
   label: string;
   to: string;
 };
 
+const Link = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 48px;
+  border-radius: 4px;
+  background: #39e09b;
+  font-size: 14px;
+  text-align: center;
+  color: #263238;
+  transition: filter 0.25s ease-in;
+  &:hover {
+    filter: invert(1);
+  }
+`;
+
 const ClassicLink = (props: ClassicLinkProps) => {
-  return <a href={props.to}>{props.label}</a>;
+  return <Link href={props.to}>{props.label}</Link>;
 };
 
 export default ClassicLink;

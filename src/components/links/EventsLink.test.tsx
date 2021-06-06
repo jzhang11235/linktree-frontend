0b334a536650b041812c8d34ba0e2bdea7d31308
Jeffrey Zhang <jzhang11235@gmail.com>
2021-06-06 16:50:13 +1000

    Implement event link content and styling

--- src/components/links/EventsLink.test.tsx
+++ src/components/links/EventsLink.test.tsx
@@ -9,7 +9,7 @@ const events: SongkickEvent[] = [
     date: 'Apr 01 2019',
     location: 'The Forum, Melbourne',
     available: true,
-    url: '#',
+    url: 'https://example.com/',
   },
   {
     id: 'b',
@@ -26,8 +26,31 @@ it('renders label from prop', () => {
   expect(screen.getByRole('button')).toHaveTextContent(label);
 });
 
-it('expands event list on click', () => {
+it('expands and collapses event list on click', () => {
   render(<EventsLink label="label" events={events} />);
   userEvent.click(screen.getByRole('button'));
   expect(screen.getByRole('list')).toBeInTheDocument();
+  userEvent.click(screen.getByRole('button'));
+  expect(screen.queryByRole('list')).not.toBeInTheDocument();
+});
+
+it('renders events from prop as list items', () => {
+  render(<EventsLink label="label" events={events} />);
+  userEvent.click(screen.getByRole('button'));
+  expect(screen.getAllByRole('listitem').length).toBe(2);
+});
+
+it('renders event info', () => {
+  render(<EventsLink label="label" events={events} />);
+  userEvent.click(screen.getByRole('button'));
+  const firstEvent = screen.getAllByRole('listitem')[0];
+  expect(firstEvent).toHaveTextContent('Apr 01 2019');
+  expect(firstEvent).toHaveTextContent('The Forum, Melbourne');
+});
+
+it('points to event url', () => {
+  render(<EventsLink label="label" events={events} />);
+  userEvent.click(screen.getByRole('button'));
+  const firstLink = screen.getAllByRole('link')[0];
+  expect(firstLink).toHaveAttribute('href', 'https://example.com/');
 });
--- src/components/links/EventsLink.tsx
+++ src/components/links/EventsLink.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import styled from 'styled-components';
 import Link from './Link';
 import { SongkickEvent } from '../../types';
+import { ReactComponent as RightArrow } from '../../assets/right-arrow.svg';
+import { ReactComponent as SongkickWordmark } from '../../assets/by-songkick-wordmark.svg';
 
 export type EventsLinkProps = {
   label: string;
@@ -10,7 +12,42 @@ export type EventsLinkProps = {
 
 const ExpandableLinkContainer = styled.div`
   border-radius: 4px;
-  background: #dadee0;
+  background: #f5f7f8;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding-left: 16px;
+  li {
+    list-style: none;
+  }
+`;
+
+const EventLink = styled.a`
+  height: 72px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-right: 16px;
+  border-bottom: 1px solid #dadee0;
+  color: inherit;
+`;
+
+const EventDate = styled.div`
+  font-size: 16px;
+  line-height: 28px;
+`;
+
+const EventLocation = styled.div`
+  font-size: 12px;
+  line-height: 24px;
+`;
+
+const WordmarkContainer = styled.div`
+  height: 48px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const EventsLink = (props: EventsLinkProps) => {
@@ -22,11 +59,24 @@ const EventsLink = (props: EventsLinkProps) => {
         {props.label}
       </Link>
       {showEvents && (
-        <ul>
-          {props.events.map(event => (
-            <li key={event.id}>{event.location}</li>
-          ))}
-        </ul>
+        <>
+          <List>
+            {props.events.map(event => (
+              <li key={event.id}>
+                <EventLink href={event.url} target="_blank">
+                  <div>
+                    <EventDate>{event.date}</EventDate>
+                    <EventLocation>{event.location}</EventLocation>
+                  </div>
+                  <RightArrow />
+                </EventLink>
+              </li>
+            ))}
+          </List>
+          <WordmarkContainer>
+            <SongkickWordmark />
+          </WordmarkContainer>
+        </>
       )}
     </ExpandableLinkContainer>
   );
